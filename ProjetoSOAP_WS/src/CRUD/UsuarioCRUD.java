36b9ae5f9a19a5fbb5a3c7c26ce5bb306c33094b
Téo <resendendetto@yahoo.com.br>
2018-04-06 17:15:36 -0300

    Driver Mysql

--- ProjetoSOAP_WS/src/CRUD/UsuarioCRUD.java
+++ ProjetoSOAP_WS/src/CRUD/UsuarioCRUD.java
@@ -30,14 +30,37 @@ public class UsuarioCRUD {
 		return resposta;
 	}
 	
+	public boolean alteraUsuario(Usuario alteraUsuario) {
+		boolean resposta = true;
+		String sql = "UPDATE usuarios SET"
+				+ " usuario=?, senha=?, nome=?, sexo=?,"
+				+ " email=?, nivel=? WHERE idUsuario = ?";
+		try {
+			PreparedStatement stmt = Banco.getConexao().prepareStatement(sql);
+			stmt.setString(1,alteraUsuario.getUsuario());
+			stmt.setString(2,alteraUsuario.getSenha());
+			stmt.setString(3,alteraUsuario.getNome());
+			stmt.setString(4,alteraUsuario.getSexo());
+			stmt.setString(5,alteraUsuario.getEmail());
+			stmt.setInt(   6,alteraUsuario.getNivel());
+			stmt.setInt(   7,alteraUsuario.getId());
+			resposta = stmt.execute();
+			stmt.close();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return resposta;
+	}
+	
 	public static void main(String[] args) {
 		Usuario novo = new Usuario();
-		novo.setUsuario("seila");
-		novo.setSenha("123");
-		novo.setNome("Meu nome");
+		novo.setUsuario("teo");
+		novo.setSenha("minhasenha");
+		novo.setNome("Francisco teófilo de Resende Netto");
 		novo.setSexo("M");
-		novo.setEmail("oi@oi.com.oi");
-		novo.setNivel(100);
+		novo.setEmail("resendenetto@yahoo.com.br");
+		novo.setNivel(1);
 		
 		if(new UsuarioCRUD().novoUsuario(novo)) {
 			System.out.println("Erro ao cadastrar o novo usuário!");
